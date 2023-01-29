bfa10b72dd40b41f574344a228c186903a214e4b
Yarden Shoham <hrsi88@gmail.com>
2023-01-29 15:46:53 +0200

    Add secret for webhook (#25)
    Closes #24

--- src/webhook.ts
+++ src/webhook.ts
@@ -3,16 +3,24 @@ import { triggerBackportAction } from "./github.ts";
 
 if (
   Deno.env.get("GITEA_FORK") === undefined ||
-  Deno.env.get("GITHUB_TOKEN") === undefined
+  Deno.env.get("GITHUB_TOKEN") === undefined ||
+  Deno.env.get("WEBHOOK_SECRET_SHA256") === undefined
 ) {
-  console.log("GITEA_FORK and GITHUB_TOKEN must be set");
+  console.log("Missing environment variables");
   Deno.exit(1);
 }
 
 serve(async (req: Request) => {
   if (req.url.endsWith("/trigger")) {
-    await triggerBackportAction();
-    return Response.json({ message: "Triggered backport" });
+    if (
+      req.headers.get("X-Hub-Signature-256") ===
+        "sha256=" + Deno.env.get("WEBHOOK_SECRET_SHA256")
+    ) {
+      await triggerBackportAction();
+      return Response.json({ message: "Triggered backport" });
+    } else {
+      return Response.json({ message: "Invalid signature" });
+    }
   } else {
     return Response.json({ status: "OK" });
   }
