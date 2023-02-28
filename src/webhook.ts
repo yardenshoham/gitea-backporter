0a362985daab9e93f0b8be580eeae8e0669e35e0
Yarden Shoham <hrsi88@gmail.com>
2023-02-28 13:44:44 +0200

    Log request

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,9 +1,7 @@
 import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 
-serve((req: Request) => {
-  if (req.url.endsWith("/trigger")) {
-    return Response.json({ message: "Triggered backport" });
-  } else {
-    return Response.json({ status: "OK" });
-  }
+serve(async (req: Request) => {
+  const json = await req.json();
+  console.log(json);
+  return Response.json({ status: "OK" });
 });
