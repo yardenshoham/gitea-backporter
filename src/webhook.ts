09004bab21a00641b2f57935ed4f86001c20309e
Yarden Shoham <hrsi88@gmail.com>
2023-02-28 17:07:40 +0200

    Don't run automatically on commit (#56)
    Only manually.
    
    No more webhooks means:
    - Closes #34 
    - Closes #42

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,15 +1,7 @@
 import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
-import { triggerBackportAction } from "./github.ts";
-
-if (Deno.env.get("BACKPORTER_GITHUB_TOKEN") === undefined) {
-  console.error("BACKPORTER_GITHUB_TOKEN is not set");
-}
 
 serve(async (req: Request) => {
-  if (req.url.endsWith("/trigger")) {
-    await triggerBackportAction();
-    return Response.json({ message: "Triggered backport" });
-  } else {
-    return Response.json({ status: "OK" });
-  }
+  const json = await req.json();
+  console.log(json);
+  return Response.json({ status: "OK" });
 });
